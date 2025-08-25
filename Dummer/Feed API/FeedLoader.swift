21160036386c6b193fd2abc463a51f8c9f070060
Catarina Serrano <marialacerda@cc.ci.ufpb.br>
2025-08-25 17:35:17 -0300

    #3 networking

--- Dummer/Feed API/FeedLoader.swift
+++ Dummer/Feed API/FeedLoader.swift
@@ -4,20 +4,35 @@ protocol FeedLoader {
     func load(completion: @escaping (Result<[DummerFeedItem], Error>) -> Void)
 }
 
+public protocol HTTPClient {
+    func get(
+        from url: URL,
+        completion: @escaping (Result<HTTPURLResponse, RemoteFeedLoader.Error>) -> Void
+    )
+}
+
 public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
     
     public init(url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
     }
     
-    public func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
+                completion(.invalidData)
+            case .failure:
+                completion(.connectivity)
+            }
+        }
     }
-}
-
-public protocol HTTPClient {
-    func get(from url: URL)
 }
--- DummerTests/RemoteFeedLoaderTests.swift
+++ DummerTests/RemoteFeedLoaderTests.swift
@@ -13,7 +13,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(filePath: "https://blablabla123.com")
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
+        sut.load { _ in }
         
         // Needs to assert how MANY times we are calling this
         XCTAssertEqual(client.requestedURLs, [url])
@@ -23,12 +23,40 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(filePath: "https://blablabla123.com")
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
         
         // Needs to assert how MANY times we are calling this
         XCTAssertEqual(client.requestedURLs, [url, url])
-        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        var caputuredErrors = [RemoteFeedLoader.Error]()
+        sut.load {
+            caputuredErrors.append($0)
+        }
+        let clientError = RemoteFeedLoader.Error.connectivity
+        client.complete(with: clientError)
+        
+        XCTAssertEqual(caputuredErrors, [.connectivity])
+    }
+    
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load {
+                capturedErrors.append($0)
+            }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     // MARK: - Helpers
@@ -41,14 +69,40 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        // var requestedURL: URL?
         // Why use an array?
         // Assert value, quantity and quality at the same time
-        var requestedURLs = [URL]()
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
         
-        func get(from url: URL) {
-            requestedURL = url
-            requestedURLs.append(url)
+        private var messages = [
+            (url: URL, completion: (Result<HTTPURLResponse, RemoteFeedLoader.Error>) -> Void)
+        ]()
+        
+        func get(
+            from url: URL,
+            completion: @escaping (Result<HTTPURLResponse, RemoteFeedLoader.Error>) -> Void
+        ) {
+            messages.append((url: url, completion: completion))
+        }
+        
+        func complete(with error: RemoteFeedLoader.Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
+
+// Notes:
+// 1. Spies has to just capture values, do not STUB!!
+// 2. don't repeat yourself, use samples, keep it simple
